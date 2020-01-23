1a2a578cab4447b23d166be3572efade51d0e45f
Alex Mylonas <alex.a.mylonas@gmail.com>
2020-01-24 00:20:33 +0200

    Added Minefield compoment

---
+++ src/compoments/Minefield.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+import { generateField, revealNear } from '../utils/MinefieldUtils'
+
+import Square from './Square'
+export default function Minefield({ rows = 25, columns = 50, bombs = 5 }) {
+
+  // TODO check field can have that many bombs
+
+  // const Field = 
+  const [Field, setField] = useState(generateField({ rows, columns, bombs }))
+  const squareClick = (x,y) => {
+    console.log('x,y',x,y)
+    const cell = Field[x][y]
+    console.log('Field:', cell)
+    if (cell.isBomb) alert("BOOOM")
+    Field[x][y].revealed = true
+    let newField = Field
+    if (Field[x][y].nearBombs === 0) {
+      console.log('Trying to reveal near')
+      newField = revealNear(Field, x, y)
+    }
+
+    setField([...newField])
+    // checkIfGameWon()
+    // if (Field)
+
+  }
+
+  console.log('render?')
+  return <div className="minefield">
+    {Field.map((row, x) => {
+
+      return <div className="row" key={x}>
+        {row.map((cell, y) => <Square key={`${x}${y}`} x={x} y={y} cell={cell} onClick={squareClick}/>)}
+        </div>
+    })}
+    {/* <Square/> */}
+  </div>
+}
