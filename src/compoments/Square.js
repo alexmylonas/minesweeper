6acc10f539c3032426b44fe1c121d6e6529cf3af
Alex Mylonas <alex.a.mylonas@gmail.com>
2020-01-24 02:47:01 +0200

    Fixing Square comp

--- src/compoments/Square.js
+++ src/compoments/Square.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import classnames from 'classnames'
 
-export default function Square ({ onClick, x,y, cell }) {
+export default function Square ({ onClick, x,y, cell, lost }) {
 
-  const { revealed, nearBombs, isBomb, lost } = cell
+  const { revealed, nearBombs, isBomb } = cell
   const className = classnames('square', {
     'revealed': revealed,
     'bomb': isBomb & lost
   })
   return <div className={className} onClick={() => onClick(x,y)}>
-    {revealed && nearBombs !== 0 && nearBombs}
+    {revealed && nearBombs !== 0 && <span>{nearBombs}</span>}
   </div>
 }
