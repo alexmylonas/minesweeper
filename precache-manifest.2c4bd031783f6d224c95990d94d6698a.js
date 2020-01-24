4b3215ad28845103a46ac928ba99dea6feb89e1a
Alex Mylonas <alex.a.mylonas@gmail.com>
2020-01-24 03:06:24 +0200

    Updates

---
+++ precache-manifest.2c4bd031783f6d224c95990d94d6698a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e48255fa29d924460145532846180c7",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "b6474459a8147e498153",
+    "url": "/minesweeper/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "823ead55fbab23664e29",
+    "url": "/minesweeper/static/css/main.4061f1ce.chunk.css"
+  },
+  {
+    "revision": "b6474459a8147e498153",
+    "url": "/minesweeper/static/js/2.f69a48e5.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/minesweeper/static/js/2.f69a48e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "823ead55fbab23664e29",
+    "url": "/minesweeper/static/js/main.e9e23f83.chunk.js"
+  },
+  {
+    "revision": "81fac3eb777183ec555d",
+    "url": "/minesweeper/static/js/runtime-main.70dc93c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/minesweeper/static/media/logo.5d5d9eef.svg"
+  }
+]);
